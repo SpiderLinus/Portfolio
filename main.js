ae914f96d191d2429ccd46a56badbcdefdc5edc0
spiderlinus <linusschon08@gmail.com>
2025-10-07 23:10:33 +0200

    massive changes

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-const NavClick = document.getElementById("navbtn");
-const NavShow = document.getElementById("topnav");
-const dropdown1 = document.querySelector("dropdown-content");
 
-
-document.querySelector("#navbtn").addEventListener("click", function (e) {
-  e.preventDefault();
-  const dropdown = this.nextElementSibling;
-  dropdown.classList.toggle("show");
-});
-
-NavClick.addEventListener("click", () => {
-    NavClick.classList.toggle("rotated");
-    dropdown1.classList.toggle("show");
-
-});
